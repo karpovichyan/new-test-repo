17f212a4334302d570f91313a7dc7adee82e1a67
Карпович Ян Геннадьевич (4050718) <yakarpovich@dev.vtb.ru>
2022-10-13 12:41:35 +0300

    init commit

---
+++ src/main/java/com/example/newtestrepo/NewTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.newtestrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTestRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewTestRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newtestrepo/NewTestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newtestrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTestRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
